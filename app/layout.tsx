59c7f30262b8ca047be2ef349d07a046f6ef61f6
gifustudio-eng <gifustudio@gmail.com>
2026-05-11 13:34:08 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body className="bg-bg-primary text-text-primary antialiased">
         {children}
+      <Analytics />
       </body>
     </html>
   )
